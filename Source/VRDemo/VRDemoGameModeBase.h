ee6ac921d813839c9095d42838eaf193a61531bc
Rhyse Bendell <Rhyse Bendell>
2017-01-13 13:10:41 -0500

    Initialize project, add assets from two brothers project, organize file trees

---
+++ Source/VRDemo/VRDemoGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "VRDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRDEMO_API AVRDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
